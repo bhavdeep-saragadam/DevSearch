4ac336a38f9df025008f0c4350cc81e8e5f92fd1
Bhavdeep <58917780+bhavdeep-saragadam@users.noreply.github.com>
2021-08-10 16:59:08 -0400

    first commit

---
+++ projects/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render, redirect
+from .models import Project
+from .forms import ProjectForm
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
+def projects(request):
+    projects = Project.objects.all()
+
+    context = {
+        
+        'projects':projects
+    }
+    return render(request, 'projects/projects.html', context)
+
+def project(request,pk):
+        projectObj = Project.objects.get(id=pk)
+        return render(request, 'projects/single-project.html', {'project':projectObj})
+
+@login_required(login_url = "login")
+def createProject(request):
+
+    profile = request.user.profile
+
+    form = ProjectForm()
+    context = {'form':form}
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.owner = profile
+            project.save()
+            return redirect('projects')
+    return render(request, "projects/project_form.html",context)
+    
+@login_required(login_url = "login")
+
+def updateProject(request, pk):
+    
+    profile = request.user.profile 
+    project = profile.project_set.get(id=pk)
+    form = ProjectForm(instance=project)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context = {'form': form}
+    return render(request, "projects/project_form.html", context)
+@login_required(login_url = "login")
+
+def deleteProject(request, pk):
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    context = {
+        'object': project
+    }
+    return render(request, "projects/delete_template.html", context)
+
---
+++ users/views.py
@@ -0,0 +1,96 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .forms import CustomCreationForm, ProfileForm
+from .models import Profile
+
+
+# Create your views here.
+
+def loginUser(request):
+	page = 'register'
+
+	if request.user.is_authenticated:
+		return redirect('profiles')
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		try:
+			user = User.objects.get(username=username)
+		except:
+			messages.error(request,'Username does not exist')
+		
+		user = authenticate(request, username=username, password=password)
+
+		if user is not None:
+			login(request, user)
+			return redirect('profiles')
+		else:
+			messages.error(request,'Username Or Password are Incorrect')
+	return render(request, 'users/login_register.html')
+
+def logoutUser(request):
+	logout(request)
+	messages.error(request,'Successfully Logged Out')
+
+	return redirect('login')
+
+def registerUser(request):
+	page = 'register'
+	form = CustomCreationForm()
+
+	if request.method == 'POST':
+		form = CustomCreationForm(request.POST)
+		if form.is_valid():
+			user = form.save(commit=False)
+			user.username = user.username.lower()
+			user.save()
+			messages.success(request, 'Account Was Created')
+			login(request, user)
+			return redirect('profiles')
+		else:
+			messages.error(request, 'An Error Has Occurred During Registration')
+
+	context = {'page':page, 'form':form}
+	return render(request, 'users/login_register.html', context)
+
+def profiles(request):
+	profiles = Profile.objects.all()
+	context = {'profiles': profiles}
+	return render(request, 'users/profiles.html',context)
+def userProfile(request,pk):
+	profile = Profile.objects.get(id=pk)
+
+	topSkills = profile.skill_set.exclude(description__exact="")
+	otherSkills = profile.skill_set.filter(description="")
+
+	context = {
+	'profile':profile,
+		'topSkills':topSkills,
+			'otherSkills':otherSkills}
+			
+	return render(request, 'users/user-profile.html',context)
+@login_required(login_url="login")
+def userAccount(request):
+	profile = request.user.profile
+
+	skills = profile.skill_set.all()
+	
+	projects = profile.project_set.all()
+
+	context = {'profile':profile, 'skills':skills,'projects':projects}
+	return render(request,'users/account.html',context)
+
+def editAccount(request):
+	profile = request.user.profile
+	form = ProfileForm(instance=profile)
+
+	if request.method == 'POST':
+		form = ProfileForm(request.POST, request.FILES, instance=profile)
+		if form.is_valid():
+			form.save()
+		return redirect('account')
+	context = {'form':form}
+	return render(request,'users/profile_form.html', context)
